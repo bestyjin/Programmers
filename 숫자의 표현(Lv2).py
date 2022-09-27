20839457a837b359bc6fa008d2cb492c6e20359c
bestyjin <bestyjin0604@gmail.com>
2022-09-27 12:38:50 +0900

    Create 숫자의 표현(Lv2).py

---
+++ 숫자의 표현(Lv2).py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        s=0
+        while s < n:
+            s += i
+            i += 1
+        if s == n:
+            answer += 1
+        
+    return answer
