07175815765df99cfab53d0958587464ca740a37
bestyjin <bestyjin0604@gmail.com>
2022-09-27 12:38:52 +0900

    Create 올바른 괄호(Lv2).py

---
+++ 올바른 괄호(Lv2).py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0:
+                stack.pop()
+            else:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+    
