4bde8f00eb838058c9ad75ef8ffb053278ae9016
bestyjin <bestyjin0604@gmail.com>
2022-10-27 10:49:12 +0900

    Update 피보나치 수(Lv2).py

--- 피보나치 수(Lv2).py
+++ 피보나치 수(Lv2).py
@@ -1,9 +1,5 @@
 def solution(n):
-    answer = 0
-    stack = [0]*(n+1)
-    stack[0] = 0
-    stack[1] = 1
-    for i in range(2,n+1):
-        stack[i] = stack[i-2] + stack[i-1]
-
-    return stack[n]%1234567
+    a,b=0,1
+    for i in range(n):
+        a,b=b,a+b
+    return a%1234567
