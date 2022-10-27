f27410d55c4c6efcc4181f56426af24824d51b6a
bestyjin <bestyjin0604@gmail.com>
2022-10-27 09:51:50 +0900

    Update 숫자의 표현(Lv2).py

--- 숫자의 표현(Lv2).py
+++ 숫자의 표현(Lv2).py
@@ -1,11 +1,3 @@
-def solution(n):
-    answer = 0
-    for i in range(1,n+1):
-        s=0
-        while s < n:
-            s += i
-            i += 1
-        if s == n:
-            answer += 1
+def solution(n):   
         
-    return answer
+    return len([i for i in range(1,n+1,2) if n%i == 0])
